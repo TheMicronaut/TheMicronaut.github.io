3e6eb1c2e3a749f4b98ed432927c00852736e26a
Aleh <alexandros.zisis@gmail.com>
2015-09-24 23:00:11 +0200

    update script & load photos

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 echo START
 #echo STARTING grund
-call grunt
+#call grunt
 echo STARTING jekyll serve
-call jekyll serve --watch
 explorer  http://127.0.0.1:4000/
+call jekyll serve --no-watch
 echo END
