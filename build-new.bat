cac4415500b7b7775c0475e92c768601f6481c70
Aleh <alexandros.zisis@gmail.com>
2016-02-06 00:43:11 +0100

    update footer

--- build-new.bat
+++
@@ -1,2 +0,0 @@
-explorer  http://127.0.0.1:4000/
-bundle exec jekyll serve
--- build.bat
+++ build.bat
@@ -1,7 +1,2 @@
-echo START
-#echo STARTING grund
-call grunt
-echo STARTING jekyll serve
 explorer  http://127.0.0.1:4000/
-call jekyll serve --no-watch
-echo END
+bundle exec jekyll serve
