a7901c158d61655de578baa1c0cbf283d8efff14
Aleh <alexandros.zisis@gmail.com>
2017-12-19 00:13:19 +0100

    new post

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 rmdir _site /s /q
 rmdir .sass-cache /s /q
-bundle exec jekyll serve --incremental --open-url --trace --force_polling
+bundle exec jekyll serve --incremental --open-url --trace --force_polling --future 
